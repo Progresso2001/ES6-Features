ca019a88c112192746fc74556c8c0dfe9d620060
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-26 22:31:54 +0530

    usando escopo de bloco de uma função

--- destructuring_Array/func.js
+++ destructuring_Array/func.js
@@ -7,9 +7,30 @@ function main(){
             {s1: 'Python', s2: 'JavaScript', s3:'MongoDB'}
     }
     const { nome, skills: { s1: skills_1, ...s }, tools = fer } = infPessoa
-    
+
     console.log("Nome do usuario: ", nome)
     console.log("Skills principais: ", skills_1)
     console.log(s)
 }
 main()
+
+//Extrair dados usando parametro na funcao 
+
+function userId(id){
+    return id;
+}
+function pessoa({ nome, nomeCompleto: { sobrenome: apelido }}){
+    console.log(nome + " é " + apelido)
+    console.log("Mais conhecido por: " +  apelido)
+    
+}
+const user = {
+    id: 40,
+    nome: 'Joaquim',
+    nomeCompleto: {
+        sobrenome: 'DevJocas'
+    }
+}
+console.log("userId: " + userId(user.id))
+
+pessoa(user)
