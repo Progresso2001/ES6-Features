e80fa82301ec56610fb55668e2d2d10acf6b5afe
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-27 19:06:25 +0530

    eliminando comentários

--- main_pratica.js
+++ main_pratica.js
@@ -1,16 +1,16 @@
-// const linguagenFavouritas = ['Python', 'JavaScript', 'MySQL', 'Node.js', 'Express.js']
-// const escolherLinguagens =  linguagenFavouritas[0]
-// if(escolherLinguagens === undefined){
-//     console.log("Não existe valor neste indice: ", escolherLinguagens)
-// }else{
-//     console.log("Existe o valor deste indice correto: ",escolherLinguagens)
-// }
+const linguagenFavouritas = ['Python', 'JavaScript', 'MySQL', 'Node.js', 'Express.js']
+const escolherLinguagens =  linguagenFavouritas[0]
+if(escolherLinguagens === undefined){
+    console.log("Não existe valor neste indice: ", escolherLinguagens)
+}else{
+    console.log("Existe o valor deste indice correto: ",escolherLinguagens)
+}
 
 // usando desestruturação de Arrays
 // Nota: A desestruturação reduz o uso de codigo e mantém o codigo mais limpo e conciso
-// const [ escolha_0 ,escolha_1, ...outros ] = linguagenFavouritas
-// console.log("As Minhas linguiagens de programação  favourita: ", escolha_0 + ", " + escolha_1)
-// console.log("Banco de dado e frameworks favourito: ", outros)
+const [ escolha_0 ,escolha_1, ...outros ] = linguagenFavouritas
+console.log("As Minhas linguiagens de programação  favourita: ", escolha_0 + ", " + escolha_1)
+console.log("Banco de dado e frameworks favourito: ", outros)
 
 // usando objectos
 const minhasLinguagens = {
@@ -22,10 +22,11 @@ const minhasLinguagens = {
     }
 }
 
-// const { nome, linguagens: {1: primeiraFavourita, 2:segundaFavourita}} = minhasLinguagens
-// console.log(`Olá, chamo-me: ${nome} e gosto de estudar ${primeiraFavourita}`)
-// console.log("Tenho como outra linguagem favourita: ", segundaFavourita)
+const { nome, linguagens: {1: primeiraFavourita, 2:segundaFavourita}} = minhasLinguagens
+console.log(`Olá, chamo-me: ${nome} e gosto de estudar ${primeiraFavourita}`)
+console.log("Tenho como outra linguagem favourita: ", segundaFavourita)
 
+// usando função
 function escolherLinguagem( { nome, linguagens: { 3:bancoDeDados, 4:node}}){
     console.log(`Olá, chamo-me: ${nome}`)
     console.log(`Estou a estudar: ${bancoDeDados}`)
